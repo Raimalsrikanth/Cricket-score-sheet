9b255d4d4607a4b2cfc376166a51b146ec274700
Raimalsrikanth <96488129+Raimalsrikanth@users.noreply.github.com>
2021-12-31 20:19:52 +0530

    Created i.c
    This prints the output on the screen

---
+++ i.c
@@ -0,0 +1,24 @@
+#include"declaration.h"
+#include"Welcome.h"
+#include"menu.h"
+#include"newscoresheet.h"
+int main()
+{
+int a,key;
+Char b;
+File *f new;
+Char last input;
+While (TRUE)
+{
+Welcome();
+System("Cls");
+menu();
+Ch=getch();
+Char ch;
+Switch(ch)
+{
+Case'1';
+fileopen(ch);
+Case '2';
+fileopen(ch);
+getch();
